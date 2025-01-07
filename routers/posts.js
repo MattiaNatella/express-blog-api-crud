8776c59b20ea41adaccc5bf1cf5f282e0bcdc80e
MattiaNatella <mattianatella@gmail.com>
2025-01-07 14:54:05 +0100

    First commit

---
+++ routers/posts.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const posts = require('../posts')
+
+
+//index
+router.get('/', (req,res) => {
+  res.json(posts)
+})
+
+//show
+router.get('/:title', (req,res) => {
+  const singlePost = posts.find( post => post.title == req.params.title)
+  res.json(singlePost)
+})
+
+//store
+router.post('/', (req,res) => {
+  res.send('Rotta store: Creo un nuovo post')
+})
+
+//update
+router.put('/:id', (req,res) => {
+  res.send('Rotta update: stampo tutti i posts')
+})
+
+//modify
+router.patch('/:id', (req,res) => {
+  res.send('Rotta modify: stampo tutti i posts')
+})
+
+//destroy
+router.delete('/:id', (req,res) => {
+  res.send('Rotta destroy: stampo tutti i posts')
+})
+
+module.exports =  router
